5e77a2e4209f5180e81211b9362b69d066ca1c6e
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-06 00:30:47 +0300

    add scripts

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+
+console.log(modalAuth);
+
+buttonAuth.addEventListener('click', () => {
+    console.log('click');
+})
