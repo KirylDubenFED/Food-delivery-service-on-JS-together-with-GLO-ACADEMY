e94c1059335d6d8e69817e39b60e7820e9f03d6d
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-08 01:16:51 +0300

    add scripts in menu

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,12 @@
 const restourant = 'tanuki';
+const cardsMenu = document.querySelector('.cards-menu')
+
+const renderItems = (data) => {
+    const card = document.createElement('div')
+
+    card.classList.add('card')
+    console.log(card);
+}
 
 fetch(`./db/${restourant}.json`)
     .then((response) => response.json())
