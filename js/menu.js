e1001635a0851a97e1ab857fb5d1d0e741dd85f7
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-09 00:26:42 +0300

    fix

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,25 @@ const renderItems = (data) => {
     const card = document.createElement('div')
 
     card.classList.add('card')
-    console.log(card);
+    
+    card.innerHTML = `
+        <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">Пицца Везувий</h3>
+                </div>
+                <div class="card-info">
+                    <div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+                    «Халапенье», соус «Тобаско», томаты.
+                    </div>
+                </div>
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                    <span class="button-card-text">В корзину</span>
+                    <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">545 ₽</strong>
+                </div>
+                </div>`
 }
 
 fetch(`./db/${restourant}.json`)
