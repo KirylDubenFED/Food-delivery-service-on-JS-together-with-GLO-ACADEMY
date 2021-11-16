c9e18fd814b708db4d0b60a4e2dc16f3a3234e92
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-17 00:18:16 +0300

    fix

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,43 @@
-// const restourant = 'food-band'
+const restourant = 'tanuki'
+const cardsMenu = document.querySelector('.cards-menu')
 
-// const renderItems = (data) => {
-//     console.log(data);
-//   }
-  
-//   fetch(`./db/${restourant}.json`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       renderItems(data)
-//     })
+const renderItems = (data) => {
+  data.forEach(({ description, id, image, name, price }) => {
+    const card = document.createElement('div')
+
+    card.classList.add('card')
+
+    card.innerHTML = `
+        <div class="card">
+						<img src="${image}" alt="${name}" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">${name}</h3>
+							</div>
+							<div class="card-info">
+								<div class="ingredients">${description}
+								</div>
+							</div>
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">${price} ₽</strong>
+							</div>
+						</div>
+					</div>
+        `
+cardsMenu.append(card)
+
+  });
+}
+
+fetch(`./db/${restourant}.json`)
+  .then((response) => response.json())
+  .then((data) => {
+    renderItems(data)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
--- js/partners.js
+++ js/partners.js
@@ -1,31 +1,31 @@
 const cardsRestaurants = document.querySelector('.cards-restaurants')
 
 const renderItems = (data) => {
-  data.forEach(item => {
+  data.forEach(({image, kitchen, name, price, products, stars, time_of_delivery}) => {
     const a = document.createElement('a')
 
     a.setAttribute('href', '/restaurant.html')
     a.classList.add('card')
     a.classList.add('card-restaurant"')
-
+    a.dataset.products = products
     a.innerHTML = `
-      <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image" />
+      <img src="${image}" alt="${name}" class="card-image" />
               <div class="card-text">
                 <div class="card-heading">
-                  <h3 class="card-title">Пицца плюс</h3>
-                  <span class="card-tag tag">50 мин</span>
+                  <h3 class="card-title">${name}</h3>
+                  <span class="card-tag tag">${time_of_delivery} мин</span>
                 </div>
                 <div class="card-info">
                   <div class="rating">
-                    4.5
+                  ${stars}
                   </div>
-                  <div class="price">От 900 ₽</div>
-                  <div class="category">Пицца</div>
+                  <div class="price">От ${price} ₽</div>
+                  <div class="category">${kitchen}</div>
                 </div>
               </div>
     `
 
-    console.log(a);
+   cardsRestaurants.append(a)
   });
 }
 
