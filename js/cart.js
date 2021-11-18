a22b5795dc24075e44830b3961c85de8b8e2cd55
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-19 00:13:10 +0300

    add innerHTML

--- js/cart.js
+++ js/cart.js
@@ -2,18 +2,33 @@ const cart = () => {
   const buttonCart = document.getElementById('cart-button')
   const modalCart = document.querySelector('.modal-cart')
   const close = modalCart.querySelector('.close')
+  const body = modalCart.querySelector('.modal-body')
 
   const renderItems = (data) => {
-    data.forEach(cartItem => {
-      console.log(cartItem);
+    data.forEach(({ name, price, id, count }) => {
+      const cartElem = document.createElement('div')
+
+      cartElem.classList.add('food-row')
+
+      cartElem.innerHTML = `
+      <span class="food-name">${name}</span>
+					<strong class="food-price">${price} ₽</strong>
+					<div class="food-counter">
+						<button class="counter-button btn-dec">-</button>
+						<span class="counter">${count}</span>
+						<button class="counter-button btn inc">+</button>
+					</div>
+      `
+      body.append(cartElem)
+      console.log(body);
     });
   }
 
   buttonCart.addEventListener('click', () => {
 
-if (localStorage.getItem('cart')) {
-renderItems(JSON.parse(localStorage.getItem('cart')))
-}
+    if (localStorage.getItem('cart')) {
+      renderItems(JSON.parse(localStorage.getItem('cart')))
+    }
 
     modalCart.classList.add('is-open')
   })
