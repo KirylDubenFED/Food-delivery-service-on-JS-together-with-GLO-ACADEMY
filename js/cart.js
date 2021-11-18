52dae51adfc62949b83a271eff6d7b971a6c4a57
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-19 01:40:44 +0300

    add POST

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,41 @@ const cart = () => {
   const modalCart = document.querySelector('.modal-cart')
   const close = modalCart.querySelector('.close')
   const body = modalCart.querySelector('.modal-body')
+  const buttonSend = modalCart.querySelector('.button-primary')
+
+  console.log(buttonSend);
+
+  const incrementCount = (id) => {
+    const cartArray = JSON.parse(localStorage.getItem('cart'))
+
+    cartArray.map((item) => {
+      if (item.id === id) {
+        item.count++
+      }
+
+      return item
+    })
+
+    localStorage.setItem('cart', JSON.stringify(cartArray))
+    renderItems(cartArray)
+  }
+
+  const decrementCount = (id) => {
+    const cartArray = JSON.parse(localStorage.getItem('cart'))
+
+    cartArray.map((item) => {
+      if (item.id === id) {
+        item.count = item.count > 0
+          ? item.count - 1
+          : 0
+      }
+
+      return item
+    })
+
+    localStorage.setItem('cart', JSON.stringify(cartArray))
+    renderItems(cartArray)
+  }
 
   const renderItems = (data) => {
     body.innerHTML = ''
@@ -16,17 +51,33 @@ const cart = () => {
       <span class="food-name">${name}</span>
 					<strong class="food-price">${price} ₽</strong>
 					<div class="food-counter">
-						<button class="counter-button btn-dec">-</button>
+						<button class="counter-button btn-dec" data-index="${id}">-</button>
 						<span class="counter">${count}</span>
-						<button class="counter-button btn inc">+</button>
+						<button class="counter-button btn-inc" data-index="${id}">+</button>
 					</div>
       `
-
-      
-
       body.append(cartElem)
     });
   }
+
+  body.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (e.target.classList.contains('btn-inc')) {
+      incrementCount(e.target.dataset.index)
+    } else if (e.target.classList.contains('btn-dec')) {
+      decrementCount(e.target.dataset.index)
+    }
+  })
+
+  buttonSend.addEventListener('click', () => {
+    const cartArray = localStorage.getItem('cart')
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: cartArray
+    })
+  })
 
   buttonCart.addEventListener('click', () => {
 
